e0ef662e2579a65b6fcdb0b1fba597ae404fad59
FranSanchez <fjsp02@gmail.com>
2025-05-05 20:18:44 +0200

    Login hecho

--- ggdeal-front/app/routes/login.tsx
+++ ggdeal-front/app/routes/login.tsx
@@ -1,7 +1,41 @@
 export default function Login() {
     return (
-        <div>
-            <h1>Este es el login</h1>
-        </div>
+        <section className="flex h-screen">
+            <div className="flex flex-1 justify-center align-center items-center">
+                <div className="flex flex-col items-center">
+                    <h1 className="text-4xl text-center mb-4">LOG IN</h1>
+                    <div className="w-90 h-[1px] bg-gray-400 mb-6"></div>
+                    
+                    <form className="w-full max-w-md">
+                        <div className="mb-4">
+                            <input 
+                                type="email" 
+                                placeholder="email" 
+                                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <input 
+                                type="password" 
+                                placeholder="********" 
+                                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            />
+                        </div>
+                    </form>
+                    <button className="w-full bg-secondary text-center text-white py-2 rounded-md border border-transparent hover:bg-transparent hover:border-secondary hover:text-secondary transition duration-300 ease-in-out cursor-pointer">
+                        LOG IN
+                    </button>
+                </div>
+            </div>
+            <div className="flex-1">
+            <div className="mr-8 mt-8 mb-8">
+                    <img 
+                        src="/img/img-login.png" 
+                        alt="Login illustration" 
+                        className="max-h-[80vh] w-full object-contain"
+                    />
+                </div>
+            </div>
+        </section>
     );
 }
